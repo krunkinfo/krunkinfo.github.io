4a7d4720d51aaf7d60fca6945b1462aca65f4375
krunkinfo <146662118+krunkinfo@users.noreply.github.com>
2023-10-01 17:54:00 +0000

    added mouse anim for real

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d")
 const mouse = {
     x: Infinity,
     y: Infinity,
-    r: 40
+    r: 45
 }
 window.onresize=resizeCanvas
 document.body.addEventListener("mousemove", (e)=>{
@@ -11,12 +11,30 @@ document.body.addEventListener("mousemove", (e)=>{
     mouse.y = e.clientY
 })
 
+let lastAnimStage = 0
+let invertStage = false
+let stageVel = 0.1
+
 function animate(){
     ctx.clearRect(0,0,canvas.width,canvas.height)
+
     ctx.strokeStyle="black"
     ctx.beginPath()
     ctx.arc(mouse.x,mouse.y,mouse.r,0,2*Math.PI)
+    ctx.arc(mouse.x,mouse.y,lastAnimStage,0,2*Math.PI)
     ctx.stroke()
+
+    if(invertStage){
+        lastAnimStage-=stageVel
+    }else{
+        lastAnimStage+=stageVel
+    }
+    if(lastAnimStage+1 > mouse.r){
+        invertStage = true 
+    } else if(lastAnimStage < 1){
+        invertStage = false 
+    }
+
     requestAnimationFrame(animate)
 }
 
