d505044cb088171d90b9094965f9ab35363c5e86
krunkinfo <146662118+krunkinfo@users.noreply.github.com>
2023-10-01 18:10:53 +0000

    beacon anim thing

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ const canvas = document.getElementById("bg-canvas")
 const ctx = canvas.getContext("2d")
 const mouse = {
     x: Infinity,
-    y: Infinity,
-    r: 45
+    y: Infinity
 }
 window.onresize=resizeCanvas
 document.body.addEventListener("mousemove", (e)=>{
@@ -17,19 +16,14 @@ function animate(){
     ctx.clearRect(0,0,canvas.width,canvas.height)
 
     ctx.beginPath()
-    ctx.strokeStyle="black"
-    ctx.arc(mouse.x,mouse.y,mouse.r,0,2*Math.PI)
-    ctx.stroke()
-
-    ctx.beginPath()
     ctx.strokeStyle="cyan"
     ctx.arc(mouse.x,mouse.y,lastAnimStage,0,2*Math.PI)
     ctx.stroke()
-
     lastAnimStage+=stageVel
     if(lastAnimStage+1 > window.innerWidth*1.7){
         lastAnimStage = 0
     }
+    
     requestAnimationFrame(animate)
 }
 
